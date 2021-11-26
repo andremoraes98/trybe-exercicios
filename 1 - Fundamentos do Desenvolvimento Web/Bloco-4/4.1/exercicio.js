d6bcc4bfb89afe74eaf81baa1fd2cc3ea1e472da
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2021-11-26 18:51:54 -0300

    Cumprimento do item 3

--- 1 - Fundamentos do Desenvolvimento Web/Bloco-4/4.1/exercicio.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco-4/4.1/exercicio.js
@@ -1,14 +1,39 @@
 let a = 5, b = 4, adicao, subtracao, multiplicacao, divisao, modulo
 
+/* Item 1: */
 adicao = a + b;
 subtracao = a - b;
 multiplicacao = a * b;
 divisao = a / b;
 modulo = a % b;
 
+/* Item 2: */
 if (a > b) { 
     console.log(a);
 } else { 
     console.log(b);
 }
 
+/* Item 3: */
+const c=3.14, d=1, e=3
+
+if( c > d ) {
+    if ( c > e ) { 
+        console.log(c);
+    } else {
+        console.log(e);
+    }
+}
+else if ( c < d ) { 
+    if ( d > e ) { 
+        console.log(d);
+    } else { 
+        console.log(e);
+    }
+} else { 
+    if ( c < e ) { 
+        console.log(e)
+    } else { 
+        console.log(c)
+    }
+}
