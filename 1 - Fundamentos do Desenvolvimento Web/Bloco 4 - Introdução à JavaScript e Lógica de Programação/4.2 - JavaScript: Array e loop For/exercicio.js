50115ea9d171141ef00af4d53af04277e2bea6df
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2021-11-29 15:53:41 -0300

    Cumprimento do item 3 do Exercicio de fixaçao

--- 1 - Fundamentos do Desenvolvimento Web/Bloco 4 - Introdução à JavaScript e Lógica de Programação/4.2 - JavaScript: Array e loop For/exercicio.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 4 - Introdução à JavaScript e Lógica de Programação/4.2 - JavaScript: Array e loop For/exercicio.js
@@ -1,12 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-/*Item 1: 
+/*Item 1: Imprimir os valores do array numbers
 
 for ( let indice = 0; indice < numbers.length; indice += 1 ) {
     console.log(numbers[indice])
 }
 
-Item 2: */
+Item 2: Imprimir a soma de todos os valores do vetor numbers
 let contador = 0
 
 for ( let indice = 0; indice < numbers.length; indice += 1 ) {
@@ -14,3 +14,12 @@ for ( let indice = 0; indice < numbers.length; indice += 1 ) {
 }
 
 console.log(contador)
+
+Item 3: Imprimir a média aritmética dos valores do array numbers*/
+let contador = 0
+
+for ( let indice = 0; indice < numbers.length; indice += 1 ) {
+    contador += numbers[indice]
+}
+
+console.log(contador/numbers.length)
