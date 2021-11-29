555eeea9acbb8960323a3223f89751ca4099dc0f
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2021-11-29 18:29:17 -0300

    Cumprimento do item 5 do Exercicio de fixaçao

--- 1 - Fundamentos do Desenvolvimento Web/Bloco 4 - Introdução à JavaScript e Lógica de Programação/4.2 - JavaScript: Array e loop For/exercicio.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 4 - Introdução à JavaScript e Lógica de Programação/4.2 - JavaScript: Array e loop For/exercicio.js
@@ -27,7 +27,7 @@ for ( let indice = 0; indice < numbers.length; indice += 1 ) {
 console.log(contador/numbers.length)
 
 
-Item 4: Imprimir "valor maior que 20" se a média for > 20 e "valor menor ou igual a 20" se a média for < 20*/
+Item 4: Imprimir "valor maior que 20" se a média for > 20 e "valor menor ou igual a 20" se a média for < 20
 let contador = 0
 
 for ( let indice = 0; indice < numbers.length; indice += 1 ) {
@@ -38,4 +38,21 @@ if ( contador/numbers.length > 20 ) {
     console.log("valor maior que 20")
 } else {
     console.log("valor menor ou igual a 20")
+}
+
+
+Item 5: Imprimir o maior valor do vetor*/
+let maiorNumero, contador = 0;
+
+for ( let indice = 0; indice < numbers.length; indice += 1 ) {
+    for ( let numero = 0; numero < numbers.length; numero += 1 ) {
+        if ( numbers[indice] > numbers[numero] ) {
+            contador += 1;
+        }
+        if ( contador === 9 ) { 
+            console.log(numbers[indice])
+            break;
+        }
+    }
+    contador = 0;
 }
