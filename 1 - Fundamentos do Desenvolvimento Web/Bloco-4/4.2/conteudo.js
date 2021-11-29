6f7a603e5720c71f0af4c639deefceb1465d102f
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2021-11-29 11:19:19 -0300

    Cumprimento do exercicio 3

--- 1 - Fundamentos do Desenvolvimento Web/Bloco-4/4.2/conteudo.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco-4/4.2/conteudo.js
@@ -5,8 +5,15 @@ let menuServices = menu[1];
 
 console.log(menuServices);
 
-Exercicio 2: Procure o índice do valor "Portfólio" do array menu */
+Exercicio 2: Procure o índice do valor "Portfólio" do array menu
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let indexOfPortfolio = menu.indexOf('Portfólio');
 
 console.log(indexOfPortfolio);
+
+Exercício 3: Adicione o valor "Contato" no final do array menu */
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu.push('Contato')
+
+console.log(menu);
