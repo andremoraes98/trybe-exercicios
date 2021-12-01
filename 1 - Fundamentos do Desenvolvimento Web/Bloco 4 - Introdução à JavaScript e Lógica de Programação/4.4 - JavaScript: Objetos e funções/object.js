9a0eb36a67a3356618a3424a749974ea536707ad
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2021-12-01 15:00:07 -0300

    Cumprimento dos itens 1 e 2 do exercicio

---
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 4 - Introdução à JavaScript e Lógica de Programação/4.4 - JavaScript: Objetos e funções/object.js
@@ -0,0 +1,20 @@
+/* Item 1: Crie um objeto player contendo as variáveis listadas abaixo
+let name = 'Marta';
+let lastName = 'Silva';
+let age = 34;
+let medals = { golden: 2, silver: 3 };
+*/
+
+let player = {
+    name:'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: {
+        golden: 2,
+        silver: 3
+    }
+}
+
+/* Item 2: Acesse as chaves name, lastName e age e concatene as suas informações para imprimir no console uma mensagem no seguinte formato: "A jogadora Marta Silva tem 34 anos de idade". */
+
+console.log("A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade")
