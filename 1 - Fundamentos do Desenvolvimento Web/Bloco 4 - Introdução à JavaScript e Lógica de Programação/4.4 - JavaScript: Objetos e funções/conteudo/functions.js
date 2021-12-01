dfc9e0d8c989f12a198fa17367e5124d680319a9
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2021-12-01 15:45:59 -0300

    Refatoracao do item 1 do exercicio 4.1

---
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 4 - Introdução à JavaScript e Lógica de Programação/4.4 - JavaScript: Objetos e funções/conteudo/functions.js
@@ -0,0 +1,36 @@
+/* Item 1: */
+
+// Adição:
+function adicao (a, b) {
+    let sum;
+    sum = a + b;
+    return sum
+}
+
+// Subtração (a - b)
+function adicao (a, b) {
+    let sub;
+    sub = a - b;
+    return sub
+}
+
+// Multiplicação (a * b)
+function multiplicacao (a, b) {
+    let mult;
+    mult = a * b
+    return mult
+}
+
+// Divisão (a / b)
+function divisao (a, b) {
+    let div;
+    div = a / b
+    return div
+}
+
+// Módulo (a % b)
+function modulo (a, b) {
+    let mod;
+    mod = a % b
+    return mod 
+}
