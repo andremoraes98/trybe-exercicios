657be28920cf931c946c07f35316ca5434a47c14
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2021-12-01 18:19:28 -0300

    Resolucao do item 4 e 5

--- 1 - Fundamentos do Desenvolvimento Web/Bloco 4 - Introdução à JavaScript e Lógica de Programação/4.4 - JavaScript: Objetos e funções/exercicio/parte1.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 4 - Introdução à JavaScript e Lógica de Programação/4.4 - JavaScript: Objetos e funções/exercicio/parte1.js
@@ -4,6 +4,13 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
 
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+  }
+
 /* Item 1: Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome */
 console.log("Bem vinda,", info.personagem)
 
@@ -13,7 +20,11 @@ info['recorrente'] = 'Sim'
 console.log(info)
 
 /* Item 3: Faça um for/in que mostre todas as chaves do objeto */
-
 for ( let key in info ) {
     console.log(key)
+}
+
+/* Item 4: Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto */
+for ( let key in info ) {
+    console.log(info[key], "e", info2[key])
 }
