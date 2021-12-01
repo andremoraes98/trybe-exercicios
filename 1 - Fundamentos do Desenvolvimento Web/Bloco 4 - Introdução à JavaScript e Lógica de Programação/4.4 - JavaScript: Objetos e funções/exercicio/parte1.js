7744a181f1871cee0c455d6e75a4d8df24ace7c2
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2021-12-01 18:49:07 -0300

    Resolucao do item 7

--- 1 - Fundamentos do Desenvolvimento Web/Bloco 4 - Introdução à JavaScript e Lógica de Programação/4.4 - JavaScript: Objetos e funções/exercicio/parte1.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 4 - Introdução à JavaScript e Lógica de Programação/4.4 - JavaScript: Objetos e funções/exercicio/parte1.js
@@ -34,17 +34,38 @@ Item 5: Defina um segundo objeto com a mesma estrutura (as mesmas chaves) do pri
 
 /* Item 6: Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'"*/
 
-let leitor = {
-    nome: 'Julia',
-    sobrenome: 'Pessoa',
-    idade: 21,
-    livrosFavoritos: [
-      {
-        titulo: 'O Pior Dia de Todos',
-        autor: 'Daniela Kopsch',
-        editora: 'Tordesilhas',
-      },
-    ],
-  };
+    let leitor = {
+        nome: 'Julia',
+        sobrenome: 'Pessoa',
+        idade: 21,
+        livrosFavoritos: [
+        {
+            titulo: 'O Pior Dia de Todos',
+            autor: 'Daniela Kopsch',
+            editora: 'Tordesilhas',
+        },
+        ],
+    };
 
-  console.log("O livro favorito de", leitor.nome, leitor.sobrenome, "se chama", leitor.livrosFavoritos[0].titulo)
+    console.log("O livro favorito de", leitor.nome, leitor.sobrenome, "se chama", leitor.livrosFavoritos[0].titulo)
+
+  /* Item 7: Adicione um novo livro favorito na chave livrosFavoritos , que é um array */
+    leitor = {
+        nome: 'Julia',
+        sobrenome: 'Pessoa',
+        idade: 21,
+        livrosFavoritos: [
+        {
+            titulo: 'O Pior Dia de Todos',
+            autor: 'Daniela Kopsch',
+            editora: 'Tordesilhas',
+        },
+        {
+            titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+            autor: 'JK Rowling',
+            editor: 'Rocco',
+          }
+        ],
+    };
+
+    console.log(leitor)
