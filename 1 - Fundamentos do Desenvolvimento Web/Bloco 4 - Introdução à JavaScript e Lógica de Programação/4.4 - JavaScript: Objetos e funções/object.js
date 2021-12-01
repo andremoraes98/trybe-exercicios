0b25ddd76c8b8797ee94e1060608ccfd99401813
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2021-12-01 15:02:40 -0300

    Cumprimento do item 3 do conteudo

--- 1 - Fundamentos do Desenvolvimento Web/Bloco 4 - Introdução à JavaScript e Lógica de Programação/4.4 - JavaScript: Objetos e funções/object.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 4 - Introdução à JavaScript e Lógica de Programação/4.4 - JavaScript: Objetos e funções/object.js
@@ -12,9 +12,11 @@ let player = {
     medals: {
         golden: 2,
         silver: 3
-    }
+    },
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
 }
 
 /* Item 2: Acesse as chaves name, lastName e age e concatene as suas informações para imprimir no console uma mensagem no seguinte formato: "A jogadora Marta Silva tem 34 anos de idade". */
 
 console.log("A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade")
+
