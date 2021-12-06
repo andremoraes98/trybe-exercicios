2f428e1865d1314194481ba7eaadb438d73cb3aa
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2021-12-06 16:01:14 -0300

    Requisito da Parte III incompleto

--- 1 - Fundamentos do Desenvolvimento Web/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/5.2 - Trabalhando com Elementos/conteudo/Parte III/script.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/5.2 - Trabalhando com Elementos/conteudo/Parte III/script.js
@@ -1,29 +1,10 @@
-// 1 -Crie um irmão para elementoOndeVoceEsta
-let paiElementoOndeVoceEsta = document.querySelector("#pai")
+// Remova todos os elementos filhos de paiDoPai exceto pai , elementoOndeVoceEsta e primeiroFilhoDoFilho .
+let familia = document.querySelectorAll("section")
 
-let sibilingElementoOndeVoceEsta = document.createElement("p")
-
-sibilingElementoOndeVoceEsta.innerText = "Irmão do elementoOndeVoceEsta"
-
-paiElementoOndeVoceEsta.appendChild(sibilingElementoOndeVoceEsta)
-
-// 2 - Crie um filho para elementoOndeVoceEsta
-let elementoOndeVoceEsta = document.querySelector("#elementoOndeVoceEsta")
-
-let childOfElementoOndeVoceEsta = document.createElement("p")
-
-childOfElementoOndeVoceEsta.innerText = "Filho do elementoOndeVoceEsta"
-
-elementoOndeVoceEsta.appendChild(childOfElementoOndeVoceEsta)
-
-// 3 - Crie um filho para primeiroFilhoDoFilho
-let primeiroFilhoDoFilho = document.querySelector("#primeiroFilhoDoFilho")
-
-let childOfPrimeiroFilhoDoFilho = document.createElement("p")
-
-childOfPrimeiroFilhoDoFilho.innerText = "Filho do primeiroFilhoDoFilho"
-
-primeiroFilhoDoFilho.appendChild(childOfPrimeiroFilhoDoFilho)
-
-// 4 - A partir desse filho criado, acesse terceiroFilho
-console.log(document.querySelector("p").parentNode.parentNode.nextElementSibling)
+for ( let index = 0; index < familia.length; index += 1 ) {
+    element = familia[index]
+    if ( element.id !== "pai" && element.id !== "elementoOndeVoceEsta" && element.id !== "primeiroFilhoDoFilho") {
+        //familia.removeChild(element)
+        console.log(element)
+    }
+}
