e1da9735541b6a3ff63e30a3f8aa7edc0812ae41
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2021-12-06 19:04:25 -0300

    Cumprimento do item 7

--- 1 - Fundamentos do Desenvolvimento Web/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/5.2 - Trabalhando com Elementos/exercicio/script.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/5.2 - Trabalhando com Elementos/exercicio/script.js
@@ -35,6 +35,15 @@ insertChildOn(getHTMLTag('main'), creatTag('','section', 'left-content'))
 // 6 - Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
 insertChildOn(getHTMLTag('main'), creatTag('', 'section', 'right-content'))
 
-// Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do section criado no passo 5;
+// 7 - Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do section criado no passo 5;
+function getSpecificHTMLTag(tag, posicao) {
+    let elements = document.querySelectorAll(tag)
+    return elements[posicao-1]
+}
+
+insertChildOn(getSpecificHTMLTag('section', 3), creatTag('', 'img', 'small-image'))
+
+getHTMLTag('img').setAttribute("src", "https://picsum.photos/200")
+
 // Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um , dois , três , ... como valores da lista. Essa lista deve ser filha do section criado no passo 6;
 // Adicione 3 tags h3 , todas sendo filhas do main criado no passo 2.
