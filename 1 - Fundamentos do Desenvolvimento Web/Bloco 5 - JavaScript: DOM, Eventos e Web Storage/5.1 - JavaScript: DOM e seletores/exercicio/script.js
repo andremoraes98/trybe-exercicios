1a3a89e2a554e61a4b9fc733b1b61a4417308c80
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2021-12-06 11:02:59 -0300

    Alteracao do JavaScript do exercicio

--- 1 - Fundamentos do Desenvolvimento Web/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/5.1 - JavaScript: DOM e seletores/exercicio/script.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/5.1 - JavaScript: DOM e seletores/exercicio/script.js
@@ -13,7 +13,6 @@ function alteraClassFuture() {
 
 alteraClassFuture();
 
-
 //2. Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
 document.getElementsByClassName("main-content")[0].style.backgroundColor = "rgb(76,164,109)"
 
