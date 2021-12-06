a64b8588451e2afa0065c247971d0341f780b74b
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2021-12-06 18:23:44 -0300

    Commit Inicial dos exercicios com resolucao do item 1

--- 1 - Fundamentos do Desenvolvimento Web/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/5.2 - Trabalhando com Elementos/exercicio/script.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/5.2 - Trabalhando com Elementos/exercicio/script.js
@@ -9,9 +9,13 @@ function insertChildOn (parent, child) {
     parent.appendChild(child)
 }
 
-insertChildOn(document.querySelector('body'), creatTag('hello','p'))
+insertChildOn(document.querySelector('body'), creatTag('Exercício 5.2 - JavaScript DOM','h1'))
 
-// Adicione a tag main com a classe main-content como filho da tag body ;
+// 2 - Adicione a tag main com a classe main-content como filho da tag body
+function insertClassOn (class, tag) {
+
+}
+
 // Adicione a tag section com a classe center-content como filho da tag main criada no passo 2;
 // Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
 // Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
