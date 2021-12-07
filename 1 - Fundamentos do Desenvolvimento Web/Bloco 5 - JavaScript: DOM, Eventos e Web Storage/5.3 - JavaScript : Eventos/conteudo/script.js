1c49fbd22ac0ae867047531630c2fcc442da5d7a
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2021-12-07 15:57:22 -0300

    Cumprimento parcial dos itens 3 e 4

--- 1 - Fundamentos do Desenvolvimento Web/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/5.3 - JavaScript : Eventos/conteudo/script.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/5.3 - JavaScript : Eventos/conteudo/script.js
@@ -24,15 +24,26 @@ selectTagNodeList(3).addEventListener("click", adicionaClasseTech)
 
 function adicionaClasseTech (eventoDeOrigem) {
     eventoDeOrigem.target.className = "tech"
-    console.log(eventoDeOrigem.target)
 }
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
+let button = document.getElementById("input")
+
+button.addEventListener("", adicionaClasseTech)
+
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+
+let dbClickH3 = document.querySelector("h3")
+
+dbClickH3.addEventListener("dblclick", abrePortifolio)
+
+function abrePortifolio() {
+
+}
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
