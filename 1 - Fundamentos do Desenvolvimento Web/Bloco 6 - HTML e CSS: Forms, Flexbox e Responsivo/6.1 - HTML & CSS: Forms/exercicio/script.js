fa24592b296d6cb320d24d2cb3f9bb138334f1a4
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-01-04 15:57:44 -0300

    Criaçao do select com todos os estados brasileiros

---
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 6 - HTML e CSS: Forms, Flexbox e Responsivo/6.1 - HTML & CSS: Forms/exercicio/script.js
@@ -0,0 +1,9 @@
+const statesList = document.querySelector('#estado');
+const statesBR = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal'];
+
+
+for (let index = 0; index < statesBR.length; index += 1) {
+  const options = document.createElement('option');
+  options.innerHTML = statesBR[index];
+  statesList.appendChild(options);
+}
