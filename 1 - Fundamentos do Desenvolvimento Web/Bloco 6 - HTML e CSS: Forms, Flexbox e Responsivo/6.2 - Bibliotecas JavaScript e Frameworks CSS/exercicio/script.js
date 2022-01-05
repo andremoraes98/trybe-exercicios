ed35b6d66a95476ede0188b024338a46e7c5ced8
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-01-05 19:43:07 -0300

    Conclusao da validaçao do fieldset 1

--- 1 - Fundamentos do Desenvolvimento Web/Bloco 6 - HTML e CSS: Forms, Flexbox e Responsivo/6.2 - Bibliotecas JavaScript e Frameworks CSS/exercicio/script.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 6 - HTML e CSS: Forms, Flexbox e Responsivo/6.2 - Bibliotecas JavaScript e Frameworks CSS/exercicio/script.js
@@ -3,3 +3,63 @@ const date = document.querySelector('#Data');
 date.DatePickerX.init({
   format: 'dd/mm/yyyy'
 });
+
+const validation = new JustValidate('#form');
+
+validation
+  .addField('#Nome', [
+    {
+      rule: 'required',
+      errorMessage: 'Nome é obrigatório.',
+    },
+    {
+      rule: 'maxLength',
+      value: 40,
+    },
+  ])
+  .addField('#E-mail', [
+    {
+      rule: 'required',
+      errorMessage: 'E-mail é obrigatório.',
+    },
+    {
+      rule: 'maxLength',
+      value: 50,
+    },
+  ])
+  .addField('#CPF', [
+    {
+      rule: 'required',
+      errorMessage: 'CPF é obrigatório.',
+    },
+    {
+      rule: 'maxLength',
+      value: 11,
+    },
+  ])
+  .addField('#Endereço', [
+    {
+      rule: 'required',
+      errorMessage: 'Endereço é obrigatório.',
+    },
+    {
+      rule: 'maxLength',
+      value: 200,
+    },
+  ])
+  .addField('#Cidade', [
+    {
+      rule: 'required',
+      errorMessage: 'Cidade é obrigatório.',
+    },
+    {
+      rule: 'maxLength',
+      value: 28,
+    },
+  ])
+  .addField('#Estado', [
+    {
+      rule: 'required',
+      errorMessage: 'Estado é obrigatório.',
+    },
+  ])
