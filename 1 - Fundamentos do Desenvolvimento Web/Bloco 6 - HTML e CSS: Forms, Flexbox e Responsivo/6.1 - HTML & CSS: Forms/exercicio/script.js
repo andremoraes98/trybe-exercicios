c0be373474f6a42afc458795cff704b18be534bf
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-01-05 09:58:41 -0300

    Conclusao da validaçao e adiçao de estilo

--- 1 - Fundamentos do Desenvolvimento Web/Bloco 6 - HTML e CSS: Forms, Flexbox e Responsivo/6.1 - HTML & CSS: Forms/exercicio/script.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 6 - HTML e CSS: Forms, Flexbox e Responsivo/6.1 - HTML & CSS: Forms/exercicio/script.js
@@ -2,25 +2,44 @@ const statesList = document.querySelector('#Estado');
 const statesBR = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal'];
 const sendButton = document.querySelector('#send');
 const resetButton = document.querySelector('#reset');
+const input = document.querySelectorAll('input');
 
 for (let index = 0; index < statesBR.length; index += 1) {
   const options = document.createElement('option');
   options.innerHTML = statesBR[index];
+  options.value = statesBR[index];
   statesList.appendChild(options);
 }
 
-sendButton.addEventListener('click', creatDivWithDatas);
+sendButton.addEventListener('click', validDados);
 
-function creatDivWithDatas () {
+function validDados() {
+  let dadosValidos = 0;
+  let dadosInvalidos = '';
+  event.preventDefault();
+  for (let index = 0; index < input.length; index += 1) {
+    if (input[index].value !== '') {
+      dadosValidos += 1;
+    } else {
+      dadosInvalidos += input[index].id + ' ';
+    }
+    console.log(dadosValidos, input.length);
+    if (input.length === dadosValidos) {
+      validDate();
+    } else {
+      
+    }
+  }
+}
+
+function validDate() {
   const dataDiv = document.querySelector('#dados');
-  const input = document.querySelectorAll('input');
   const dataInicial = document.querySelector('#Data').value;
   let dia = '';
   let mes = '';
   let ano = '';
 
   for (let index = 0; index < dataInicial.length; index += 1) {
-    
     if (dataInicial[index] !== '/') {
       if (index < 2) {
         dia += dataInicial[index];
@@ -31,6 +50,7 @@ function creatDivWithDatas () {
       }
     }
   }
+
   if (parseInt(dia) < 0 || parseInt(dia) > 31) {
     alert('Dia inválido');
   } else if (parseInt(mes) < 0 || parseInt(mes) > 12 ) {
@@ -41,7 +61,6 @@ function creatDivWithDatas () {
     for (let index = 0; index < input.length; index += 1) {
       const newDiv = document.createElement('div');
       newDiv.innerHTML = input[index].id + ': ' + input[index].value;
-      console.log(newDiv);
       dataDiv.appendChild(newDiv);
     }
   }
