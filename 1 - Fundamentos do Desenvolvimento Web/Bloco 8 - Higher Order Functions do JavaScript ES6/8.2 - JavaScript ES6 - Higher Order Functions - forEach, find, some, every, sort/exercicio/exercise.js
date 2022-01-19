fb0319ec936962fd93988cb6abf443d5c1ebe102
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-01-19 19:26:35 -0300

    Cumprimento do requisito 5 do exercicio

--- 1 - Fundamentos do Desenvolvimento Web/Bloco 8 - Higher Order Functions do JavaScript ES6/8.2 - JavaScript ES6 - Higher Order Functions - forEach, find, some, every, sort/exercicio/exercise.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 8 - Higher Order Functions do JavaScript ES6/8.2 - JavaScript ES6 - Higher Order Functions - forEach, find, some, every, sort/exercicio/exercise.js
@@ -156,3 +156,12 @@ function booksOrderedByReleaseYearDesc() {
     return 0
   });
 }
+
+
+// 5 - Faça uma função que retorne true , se todas as pessoas autoras nasceram no século XX, ou false , caso contrário.
+// const expectedResult = false;
+
+function everyoneWasBornOnSecXX() {
+  // escreva seu código aqui
+  return books.every((book) => book.author.birthYear > 1900 && book.author.birthYear <= 2000)
+}
