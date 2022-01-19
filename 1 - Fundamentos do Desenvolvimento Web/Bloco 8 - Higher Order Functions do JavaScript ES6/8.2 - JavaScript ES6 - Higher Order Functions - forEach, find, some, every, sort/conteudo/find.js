a5e59b94140b1583dbbb6c267dc68fb60343f8dc
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-01-19 15:15:24 -0300

    Cumprimento do exercicio 2 do conteudo find

--- 1 - Fundamentos do Desenvolvimento Web/Bloco 8 - Higher Order Functions do JavaScript ES6/8.2 - JavaScript ES6 - Higher Order Functions - forEach, find, some, every, sort/conteudo/find.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 8 - Higher Order Functions do JavaScript ES6/8.2 - JavaScript ES6 - Higher Order Functions - forEach, find, some, every, sort/conteudo/find.js
@@ -1,4 +1,4 @@
-// Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5 , caso ele exista:
+// 1 -Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5 , caso ele exista:
 
 const numbers = [19, 21, 30, 3, 45, 22, 15];
 
@@ -8,3 +8,15 @@ const findDivisibleBy3And5 = (number) => {
 };
 
 console.log(numbers.find(findDivisibleBy3And5));
+
+
+// 2 - Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = (nome) => {
+  // Adicione seu código aqui:
+  return nome.length === 5
+}
+
+console.log(names.find(findNameWithFiveLetters));
