3d4bc9491d248ba2db44004d92ce5f9d7fa4b4b8
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-01-19 20:23:23 -0300

    Cumprimento parcial do item 7

--- 1 - Fundamentos do Desenvolvimento Web/Bloco 8 - Higher Order Functions do JavaScript ES6/8.2 - JavaScript ES6 - Higher Order Functions - forEach, find, some, every, sort/exercicio/exercise.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 8 - Higher Order Functions do JavaScript ES6/8.2 - JavaScript ES6 - Higher Order Functions - forEach, find, some, every, sort/exercicio/exercise.js
@@ -175,12 +175,11 @@ function someBookWasReleaseOnThe80s() {
   return books.some((book) => book.releaseYear >= 1980 && book.releaseYear < 1990)
 }
 
-console.log(someBookWasReleaseOnThe80s());
-
 
 // 7 - Faça uma função que retorne true , caso nenhum author tenha nascido no mesmo ano, e false , caso contrário.
 // const expectedResult = false;
 
 function authorUnique() {
   // escreva seu código aqui
+  return books.some((book, index, list) => book.author.birthYear === list.forEach((item) => item.author.birthYear))
 }
