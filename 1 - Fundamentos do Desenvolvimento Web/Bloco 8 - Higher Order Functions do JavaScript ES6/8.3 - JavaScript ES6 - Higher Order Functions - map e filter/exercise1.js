28aedb3db834fce09a3e3c2361c28a1b7ac659a2
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-01-20 19:08:03 -0300

    Cumprimento do requisito 5 do exercicio

--- 1 - Fundamentos do Desenvolvimento Web/Bloco 8 - Higher Order Functions do JavaScript ES6/8.3 - JavaScript ES6 - Higher Order Functions - map e filter/exercise1.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 8 - Higher Order Functions do JavaScript ES6/8.3 - JavaScript ES6 - Higher Order Functions - map e filter/exercise1.js
@@ -123,3 +123,14 @@ const oldBooksOrdered =  books.filter( (book) => {
     return 0
   }
 });
+
+
+// 5 - Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
+/* const expectedResult = [
+  'Frank Herbert',
+  'George R. R. Martin',
+  'Isaac Asimov',
+  'J. R. R. Tolkien',
+]; */
+
+const fantasyOrScienceFictionAuthors = books.map( (book) => book.author.name).sort();
