6cb4dc58eacb1c5db3938dff287316348f38e9f4
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-01-20 18:35:22 -0300

    Cumprimento do requisito 1 do exercicio

--- 1 - Fundamentos do Desenvolvimento Web/Bloco 8 - Higher Order Functions do JavaScript ES6/8.3 - JavaScript ES6 - Higher Order Functions - map e filter/exercise1.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 8 - Higher Order Functions do JavaScript ES6/8.3 - JavaScript ES6 - Higher Order Functions - map e filter/exercise1.js
@@ -62,3 +62,8 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+// 1 - Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
+
+const formatBooks = books.map( (book) => `${book.name} - ${book.genre} - ${book.author.name}`);
+
+console.log(formatBooks);
