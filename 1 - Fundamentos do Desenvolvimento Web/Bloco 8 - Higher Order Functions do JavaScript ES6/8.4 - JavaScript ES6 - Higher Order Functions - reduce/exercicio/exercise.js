f8b0d17a22a48726c140842c2e819f64dd7714ef
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-01-21 18:41:02 -0300

    Cumprimento do requisito 3 do exercicio

--- 1 - Fundamentos do Desenvolvimento Web/Bloco 8 - Higher Order Functions do JavaScript ES6/8.4 - JavaScript ES6 - Higher Order Functions - reduce/exercicio/exercise.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 8 - Higher Order Functions do JavaScript ES6/8.4 - JavaScript ES6 - Higher Order Functions - reduce/exercicio/exercise.js
@@ -86,6 +86,6 @@ const reduceNames = books.reduce( (accumulator, currentValue) => `${accumulator}
 
 // 3 - Calcule a média de idade que as pessoas autoras tinham quando seus respectivos livros foram lançados.
 // const expectedResult = 43;
-function averageAge() {
-  // escreva seu código aqui
-}
+const averageAge = books.reduce( (accumulator, currentValue) => {
+  return accumulator += (currentValue.releaseYear - currentValue.author.birthYear)
+}, 0) / books.length;
