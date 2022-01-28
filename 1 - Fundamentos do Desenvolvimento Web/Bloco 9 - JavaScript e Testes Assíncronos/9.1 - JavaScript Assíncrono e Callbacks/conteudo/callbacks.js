e7acbecde2c0a0884f34467f63c3d16f6a71b73a
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-01-28 15:42:39 -0300

    Cumprimento do requisito 2 do exercicio do conteudo de Callbacks

--- 1 - Fundamentos do Desenvolvimento Web/Bloco 9 - JavaScript e Testes Assíncronos/9.1 - JavaScript Assíncrono e Callbacks/conteudo/callbacks.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 9 - JavaScript e Testes Assíncronos/9.1 - JavaScript Assíncrono e Callbacks/conteudo/callbacks.js
@@ -2,7 +2,7 @@
 const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
 const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
 
-const getUser = () => {
+/* const getUser = () => {
   const userToReturn = {
     firstName: "Ivan",
     lastName: "Ivanovich",
@@ -10,7 +10,29 @@ const getUser = () => {
   };
   // Insira o retorno da função `getUser`
   return userNationality(userToReturn);
+}; */
+
+// console.log(getUser()); complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
+// console.log(getUser()); complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
+
+
+
+// 2 - Passe, como parâmetro e como retorno, uma callback para a função getUser 
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const getUser = (callback) => {
+  setTimeout(() => {
+    const user = {
+      firstName: "Ivan",
+      lastName: "Ivanovich",
+      nationality: "Russian",
+    };
+    // Retorne a `callback` passada como parâmetro na função `getUser`
+    // Dica: você pode manter o `console.log()`
+    return console.log(callback(user));
+  }, delay());
 };
 
-console.log(getUser()); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
-console.log(getUser()); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
+getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
+getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
