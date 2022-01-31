4716c56da99041c1042cfba0377fa11d76fcaec2
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-01-31 13:37:58 -0300

    Inicio da resolucao do requisito 7 do exercicio

--- 1 - Fundamentos do Desenvolvimento Web/Bloco 9 - JavaScript e Testes Assíncronos/9.1 - JavaScript Assíncrono e Callbacks/exercicio/exercise7.test.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 9 - JavaScript e Testes Assíncronos/9.1 - JavaScript Assíncrono e Callbacks/exercicio/exercise7.test.js
@@ -1,7 +1,14 @@
 const { default: expectationResultFactory } = require("jest-jasmine2/build/expectationResultFactory");
 
 // 7 - Escreva um teste que verifique a chamada da callback de uma função uppercase , que transforma as letras de uma palavra em letras maiúsculas. Lembre-se de ter cuidado com os falso-positivos em testes assíncronos.
-const uppercase = require('uppercase');
+const arrowTest = (string) => console.log(string);
+
+const uppercase = (str, callback) => {
+  setTimeout(() => {
+    return callback(str.toUpperCase());
+  }, 500);
+};
+
 test ('Testa a função uppercase', () => {
   expect(uppercase('aaaa', arrowTest())).toBe('AAAA');
 });
