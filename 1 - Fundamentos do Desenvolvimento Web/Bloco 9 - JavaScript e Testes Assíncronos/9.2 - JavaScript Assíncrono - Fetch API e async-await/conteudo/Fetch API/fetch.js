c2a40137d754c7d28a4c6048bb7c94315a21a0f4
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-01-31 14:47:54 -0300

    Adiçao do .cacth ao codigo

--- 1 - Fundamentos do Desenvolvimento Web/Bloco 9 - JavaScript e Testes Assíncronos/9.2 - JavaScript Assíncrono - Fetch API e async-await/conteudo/Fetch API/fetch.js
+++ 1 - Fundamentos do Desenvolvimento Web/Bloco 9 - JavaScript e Testes Assíncronos/9.2 - JavaScript Assíncrono - Fetch API e async-await/conteudo/Fetch API/fetch.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
-/* Erro no console.log 
+/* Não é impresso a resposta da requisição à API 
 const fetchJoke = () => {
   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
 
@@ -10,9 +10,16 @@ const fetchJoke = () => {
 const fetchJoke = () => {
   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
 
+  /* Se algo der errado, o código quebrará.
   fetch(url)
     .then((response) => response.json())
-    .then((data) => console.log(data.value));
+    .then((data) => console.log(data.value)); */
+
+  // Com a adição do .cacth, caso haja algum erro, ele será tratado.
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data.value))
+    .catch((error) => console.log(`Algo deu errado :( \n${error}`));
 }
 
 fetchJoke();
