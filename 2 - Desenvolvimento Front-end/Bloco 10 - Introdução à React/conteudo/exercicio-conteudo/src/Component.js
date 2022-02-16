fbae42650d473be7dc835dfd110834d7148283bf
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-02-16 16:13:02 -0300

    Commit inicial do dia 1 do bloco 10 do Módulo 2

---
+++ 2 - Desenvolvimento Front-end/Bloco 10 - Introdução à React/conteudo/exercicio-conteudo/src/Component.js
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+class descriptionAboutMe extends React.Component {
+  render() {
+    return (
+    <div>
+      <h1>André Moraes</h1>
+      <p>Estudante de Desenvolvimento Web Full Stack na Trybe</p>
+    </div>
+    )
+  }
+};
+
+export default descriptionAboutMe;
