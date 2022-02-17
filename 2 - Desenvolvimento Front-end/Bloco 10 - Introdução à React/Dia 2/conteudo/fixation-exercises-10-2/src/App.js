993c1f69e7fdb7f64d75665588d9fe85ca6a634e
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-02-17 16:05:09 -0300

    Conclusao do exercicio de fixaçao do pedido

--- 2 - Desenvolvimento Front-end/Bloco 10 - Introdução à React/Dia 2/conteudo/fixation-exercises-10-2/src/App.js
+++ 2 - Desenvolvimento Front-end/Bloco 10 - Introdução à React/Dia 2/conteudo/fixation-exercises-10-2/src/App.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import './App.css';
-import UserProfile from './UserProfile';
+import Order from './Order.js'
 
 class App extends React.Component {
   
   render() {
-    const joao = {
+    const headphone = {
       id: 102,
-      name: "João",
-      email: "joao@gmail.com",
-      avatar: "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
     };
 
-    const amelia = {
+    const energyDrink = {
       id: 77,
-      name: "Amélia",
-      email: "amelia@gmail.com",
-      avatar: "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
     };
 
     return <div>
-      <UserProfile user = { joao }/>
-      <UserProfile user = { amelia }/>
+      <Order order = { headphone }/>
+      <Order order = { energyDrink }/>
     </div>
   };
 }
