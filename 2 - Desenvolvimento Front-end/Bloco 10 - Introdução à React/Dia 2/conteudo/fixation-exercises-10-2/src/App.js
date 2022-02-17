b2fe741f114634bacf848c372ee9b916257a80d2
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-02-17 16:09:16 -0300

    Conclusao do exercicio de fixacao para renderizar componentes de forma dinamica

--- 2 - Desenvolvimento Front-end/Bloco 10 - Introdução à React/Dia 2/conteudo/fixation-exercises-10-2/src/App.js
+++ 2 - Desenvolvimento Front-end/Bloco 10 - Introdução à React/Dia 2/conteudo/fixation-exercises-10-2/src/App.js
@@ -1,34 +1,26 @@
 import React from 'react';
 import './App.css';
-import Order from './Order.js'
+import UserProfile from './UserProfile'
 
 class App extends React.Component {
   
   render() {
-    const headphone = {
-      id: 102,
-      user: "cena@gmail.com",
-      product: "Razer Headphone",
-      price: {
-        value: 99.99,
-        currency: "dollars"
+    const users = [
+      {
+        id: 102,
+        name: "João",
+        email: "joao@gmail.com",
+        avatar: "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+      },
+      {
+        id: 77,
+        name: "Amélia",
+        email: "amelia@gmail.com",
+        avatar: "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
       }
-    };
+    ];
 
-    const energyDrink = {
-      id: 77,
-      user: "cena@gmail.com",
-      product: "Monster 500mL",
-      price: {
-        value: 9.99,
-        currency: "dollars"
-      }
-    };
-
-    return <div>
-      <Order order = { headphone }/>
-      <Order order = { energyDrink }/>
-    </div>
+    return <div className='App'> { users.map((user) => <UserProfile user = { user }/>) } </div>
   };
 }
 
