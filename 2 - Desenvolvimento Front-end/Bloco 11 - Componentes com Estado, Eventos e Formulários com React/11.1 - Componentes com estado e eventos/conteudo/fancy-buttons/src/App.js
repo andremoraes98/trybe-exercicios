f653f74fe32b4adb2ba51c4cf0c5e10ca9b7e3ae
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-02-21 15:41:17 -0300

    Resolucao do exercicio de fixacao de eventos do conteudo

--- 2 - Desenvolvimento Front-end/Bloco 11 - Componentes com Estado, Eventos e Formulários com React/11.1 - Componentes com estado e eventos/conteudo/fancy-buttons/src/App.js
+++ 2 - Desenvolvimento Front-end/Bloco 11 - Componentes com Estado, Eventos e Formulários com React/11.1 - Componentes com estado e eventos/conteudo/fancy-buttons/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 
+const handleClickLeft = () => {
+  console.log('Clicou na esquerda!');
+}
+
+const handleClickMid = () => {
+  console.log('Clicou no meio!');
+}
+
+const handleClickRigth = () => {
+  console.log('Clicou na direita!');
+}
+
 function App() {
   return (
     <div className="App">
-      <p>Hello!</p>
+      <button onClick={ handleClickLeft }>Esquerda</button>
+      <button onClick={ handleClickMid }>Meio</button>
+      <button onClick={ handleClickRigth }>Direita</button>
     </div>
   );
 }
