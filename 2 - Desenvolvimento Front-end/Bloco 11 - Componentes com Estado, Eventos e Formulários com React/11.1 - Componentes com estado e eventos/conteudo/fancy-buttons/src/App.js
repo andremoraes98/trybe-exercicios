0dcc0d87984ed20eae81232f9b0b60247dea786e
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-02-21 16:23:37 -0300

    Cumprimento da mudança dos estados

--- 2 - Desenvolvimento Front-end/Bloco 11 - Componentes com Estado, Eventos e Formulários com React/11.1 - Componentes com estado e eventos/conteudo/fancy-buttons/src/App.js
+++ 2 - Desenvolvimento Front-end/Bloco 11 - Componentes com Estado, Eventos e Formulários com React/11.1 - Componentes com estado e eventos/conteudo/fancy-buttons/src/App.js
@@ -7,29 +7,41 @@ class App extends React.Component {
     this.handleClickLeft = this.handleClickLeft.bind(this);
     this.handleClickMid = this.handleClickMid.bind(this);
     this.handleClickRigth = this.handleClickRigth.bind(this);
+
+    this.state = {
+      numeroDeCliques: 0
+    }
   }
 
   handleClickLeft() {
     console.log('Clicou na esquerda!');
-    console.log(this);
+    console.log(this.state.numeroDeCliques);
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques + 1
+    }));
+    console.log(this.state.numeroDeCliques);
   }
   
   handleClickMid() {
     console.log('Clicou no meio!');
-    console.log(this);
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques + 1
+    }));
   }
   
   handleClickRigth() {
     console.log('Clicou na direita!');
-    console.log(this);
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques + 1
+    }));
   }
 
   render() {
     return (
       <div className="App">
-        <button onClick={ this.handleClickLeft }>Esquerda</button>
-        <button onClick={ this.handleClickMid }>Meio</button>
-        <button onClick={ this.handleClickRigth }>Direita</button>
+        <button onClick={ this.handleClickLeft }> {this.state.numeroDeCliques} </button>
+        <button onClick={ this.handleClickMid }> {this.state.numeroDeCliques} </button>
+        <button onClick={ this.handleClickRigth }> {this.state.numeroDeCliques} </button>
       </div>
     );
   }
