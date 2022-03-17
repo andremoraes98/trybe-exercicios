03d18e26495c4ee6b51a37832bc99d1914ea81e8
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-03-17 19:16:23 -0300

    Cumprimento parcial da parte 2

--- 2 - Desenvolvimento Front-end/Bloco 14 - Testes automatizados com React Testing Library/14.2 - RTL - Mocks e Inputs/exercicios/services.js
+++ 2 - Desenvolvimento Front-end/Bloco 14 - Testes automatizados com React Testing Library/14.2 - RTL - Mocks e Inputs/exercicios/services.js
@@ -1,3 +1,3 @@
-const generateRandomNumber = () => Math.floor(Math.random() * 100)
+const generateRandomNumber = () => Math.floor(Math.random() * 100);
 
-console.log(generateRandomNumber())
+module.exports = { generateRandomNumber };
--- 2 - Desenvolvimento Front-end/Bloco 14 - Testes automatizados com React Testing Library/14.2 - RTL - Mocks e Inputs/exercicios/services.test.js
+++ 2 - Desenvolvimento Front-end/Bloco 14 - Testes automatizados com React Testing Library/14.2 - RTL - Mocks e Inputs/exercicios/services.test.js
@@ -23,3 +23,32 @@ describe('Testa a função generateRandomNumber', () => {
     expect(generateRandomNumber).toHaveBeenCalledTimes(3);
   });
 });
+
+describe('Testa a implementação mockada da generateRandomNumber', () => {
+  it('Verifica se a função generateRandomNumber foi chamada.', () => {
+    generateRandomNumber = jest.fn().mockImplementation((a, b) => a / b);
+
+    generateRandomNumber();
+
+    expect(generateRandomNumber).toHaveBeenCalled();
+  });
+
+  it('Verifica se a nova implementação de divisão foi aplicada.', () => {
+    generateRandomNumber = jest.fn().mockImplementation((a, b) => a / b);
+
+    expect(generateRandomNumber(8, 4)).toBe(2);
+  });
+
+  it('Verifique se a aplicação da nova implementação acontece apenas uma vez.', () => {
+    const randomNumber = require('./services');
+
+    const mockGenerateRandomNumber = jest.spyOn(randomNumber, 'generateRandomNumber').mockImplementation((a, b) => a / b);
+
+    randomNumber.generateRandomNumber(8, 4);
+    expect(mockGenerateRandomNumber).toHaveBeenCalled();
+    expect(mockGenerateRandomNumber(8, 4)).toBe(2);
+
+    mockGenerateRandomNumber.mockRestore();
+    expect(generateRandomNumber(8, 4)).toBe(12);
+  });
+});
