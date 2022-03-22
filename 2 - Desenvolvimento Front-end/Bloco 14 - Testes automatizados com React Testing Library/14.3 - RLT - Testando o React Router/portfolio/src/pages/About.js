65c3f6618250720acf9f89b59899cbe1f4369a04
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-03-21 22:59:50 -0300

    Adiçao de estilizaçao do About

--- 2 - Desenvolvimento Front-end/Bloco 14 - Testes automatizados com React Testing Library/14.3 - RLT - Testando o React Router/portfolio/src/pages/About.js
+++ 2 - Desenvolvimento Front-end/Bloco 14 - Testes automatizados com React Testing Library/14.3 - RLT - Testando o React Router/portfolio/src/pages/About.js
@@ -8,18 +8,19 @@ class About extends React.Component {
         <h1>About</h1>
         <section className='about'>
           <div className='textAbout'>
-            <h2>André Luis Oliveira Rocha de Moraes</h2>
-            <p>Aspirante a desenvolvedor web full stack, fui nascido e criado em Salvador, Bahia.</p>
-            <p>Das tecnologias e linguagens que aprendi na Trybe, as que mais me orgulho são essas:</p>
-            <ul>
-              <li>HTML;</li>
-              <li>CSS;</li>
-              <li>JavaScript;</li>
-              <li>React;</li>
-              <li>React Native;</li>
-              <li>GitHub;</li>
-            </ul>
-            <p>Além disso, me orgulho de ser uma pessoa comunicadora, que sabe trabalhar em equipe, resolutiva e que AMA codar. <i>Amo tanto que até perco a hora, às vezes...</i></p>
+            <div>
+              <h2>André Luis Oliveira Rocha de Moraes</h2>
+              <p>Aspirante a desenvolvedor web full stack, fui nascido e criado em Salvador, Bahia.</p>
+              <p>Das tecnologias e linguagens que aprendi na Trybe, as que mais me orgulho são essas:</p>
+              <ul>
+                <li>HTML;</li>
+                <li>CSS;</li>
+                <li>JavaScript;</li>
+                <li>React;</li>
+                <li>GitHub;</li>
+              </ul>
+              <p>Além disso, me orgulho de ser uma pessoa comunicadora, que sabe trabalhar em equipe, resolutiva e que AMA codar. <i>Amo tanto que até perco a hora, às vezes...</i></p>
+            </div>
           </div>
           <img src={ personalImage } alt='author-with-arms-opened' />
         </section>
