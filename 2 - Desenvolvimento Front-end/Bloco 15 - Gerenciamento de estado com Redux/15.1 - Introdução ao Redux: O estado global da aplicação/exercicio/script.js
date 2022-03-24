b2bb29ba2d1d5f00d9852329db63c192072a5541
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-03-24 16:17:34 -0300

    Cumprimento do requisito 3

--- 2 - Desenvolvimento Front-end/Bloco 15 - Gerenciamento de estado com Redux/15.1 - Introdução ao Redux: O estado global da aplicação/exercicio/script.js
+++ 2 - Desenvolvimento Front-end/Bloco 15 - Gerenciamento de estado com Redux/15.1 - Introdução ao Redux: O estado global da aplicação/exercicio/script.js
@@ -1,13 +1,32 @@
-const Redux = require('redux');
 const ESTADO_INICIAL = {
   colors: ['white', 'black', 'red', 'green', 'blue', 'yellow'],
   index: 0,
 };
-const reducer = (state = ESTADO_INICIAL) => {
-  switch (true) {
+
+const nextColor = () => ({
+  type: "NEXT_COLOR",
+})
+
+const previousColor = () => ({
+  type: "PREVIOUS_COLOR",
+})
+
+const reducer = (state = ESTADO_INICIAL, action) => {
+  switch (action.type) {
+    case "NEXT_COLOR":
+      return {
+        ...state,
+        index: state.colors.length - 1 === state.index ? state.index : state.index + 1,
+      }
+    case "PREVIOUS_COLOR":
+      return {
+        ...state,
+        index: state.index === 0 ? state.index : state.index - 1,
+      }
     default:
       return state;
   }
 }
+
 const store = Redux.createStore(reducer);
 console.log(store.getState());
