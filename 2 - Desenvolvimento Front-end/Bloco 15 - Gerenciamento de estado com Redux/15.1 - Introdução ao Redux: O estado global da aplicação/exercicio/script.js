6d2b257f4ef14df9a9f5c9f1393d32d5a130b6b3
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-03-24 18:45:09 -0300

    Conclusao do exercicio

--- 2 - Desenvolvimento Front-end/Bloco 15 - Gerenciamento de estado com Redux/15.1 - Introdução ao Redux: O estado global da aplicação/exercicio/script.js
+++ 2 - Desenvolvimento Front-end/Bloco 15 - Gerenciamento de estado com Redux/15.1 - Introdução ao Redux: O estado global da aplicação/exercicio/script.js
@@ -11,8 +11,23 @@ const previousColor = {
   type: "PREVIOUS_COLOR",
 }
 
+const randomColor = {
+  type: "RANDOM_COLOR",
+}
+
+const createColor = () => {
+  const oneChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+  let cor = '#';
+  const aleatorio = () => Math.floor(Math.random() * oneChar.length);
+  for (let i = 0; i < 6; i += 1) {
+      cor += oneChar[aleatorio()];
+  }
+  return cor;
+}
+
 const nextButton = document.querySelector('#next');
 const previousButton = document.querySelector('#previous');
+const randomButton = document.querySelector('#random');
 
 const reducer = (state = ESTADO_INICIAL, action) => {
   switch (action.type) {
@@ -25,6 +40,12 @@ const reducer = (state = ESTADO_INICIAL, action) => {
       return {
         ...state,
         index: state.index === 0 ? state.index : state.index - 1,
+      }
+    case "RANDOM_COLOR":
+      return {
+        ...state,
+        colors: action.colors,
+        index: action.index,
       }
     default:
       return state;
@@ -41,13 +62,21 @@ const actionPrevious = () => {
   store.dispatch(previousColor)
 }
 
+const actionRandom = () => {
+  const { colors } = store.getState();
+  colors.push(createColor());
+  const index = colors.length - 1;
+  store.dispatch({ ...randomColor, colors, index })
+}
+
 nextButton.addEventListener('click', actionNext);
 previousButton.addEventListener('click', actionPrevious);
+randomButton.addEventListener('click', actionRandom);
 
 store.subscribe( () => {
-  const { colors, index} = store.getState();
   const colorName = document.querySelector('#value');
   const bodyContainer = document.querySelector('#container');
+  const { colors, index } = store.getState();
   colorName.innerHTML = colors[index];
   bodyContainer.style = `background-color: ${colors[index]}`
 })
