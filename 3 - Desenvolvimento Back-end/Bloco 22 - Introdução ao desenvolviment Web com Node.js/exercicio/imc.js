e01644c8a63752941b49579991a296a9fa1f602c
Andre Moraes <andre.moraes.98@gmail.com>
2022-06-06 18:47:04 -0300

    Cumprimento do requisito 2 e 3

--- 3 - Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolviment Web com Node.js/exercicio/imc.js
+++ 3 - Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolviment Web com Node.js/exercicio/imc.js
@@ -1,4 +1,6 @@
-const altura = 1.7;
-const peso = 60;
+const readLine = require('readline-sync');
+
+const altura = readLine.question('Qual a sua altura em metros? ');
+const peso = readLine.questionInt('Qual o teu peso em quilos? ');
 
 console.log(`A sua altura é ${altura}m, o teu peso é ${peso}kg e o teu IMC é ${peso / (altura ^ 2)}`);
