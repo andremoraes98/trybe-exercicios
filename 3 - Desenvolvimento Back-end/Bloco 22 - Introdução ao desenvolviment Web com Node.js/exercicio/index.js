a0418c6856ca82e36bb0062d6141a979bdbb6644
Andre Moraes <andre.moraes.98@gmail.com>
2022-06-06 19:44:25 -0300

    Cumprimento do exercicio 8

--- 3 - Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolviment Web com Node.js/exercicio/index.js
+++ 3 - Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolviment Web com Node.js/exercicio/index.js
@@ -5,11 +5,11 @@ const scriptOptions = ['velocidade', 'sorteio', 'imc'];
 const script = readLine.keyInSelect(scriptOptions, 'Qual o script desejar executar? ');
 
 if (script === 0) {
-    console.log('velocidade');
+    require('./velocidade/velocidade');
 } else if (script === 1) {
-    console.log('sorteio');
+    require('./sorteio/sorteio');
 } else if (script === 2) {
-    console.log('imc');
+    require('./IMC/imc');
 } else {
     console.log('Script inválido. Tente novamente!');
 }
