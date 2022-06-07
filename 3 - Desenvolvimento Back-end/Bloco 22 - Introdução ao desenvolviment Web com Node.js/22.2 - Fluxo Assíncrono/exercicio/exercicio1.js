471ce211d64e59f70ae78893a625f24c6a54f771
Andre Moraes <andre.moraes.98@gmail.com>
2022-06-07 16:21:46 -0300

    Cumprimento do requisito 2

--- 3 - Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolviment Web com Node.js/22.2 - Fluxo Assíncrono/exercicio/exercicio1.js
+++ 3 - Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolviment Web com Node.js/22.2 - Fluxo Assíncrono/exercicio/exercicio1.js
@@ -3,17 +3,21 @@ const fs = require('fs').promises;
 const main = (num1, num2, num3) => {
     const promise = new Promise((resolve, reject) => {
         if (
-            typeof num1 === 'number'
-            || typeof num2 === 'number'
-            || typeof num3 === 'number'
+            typeof num1 !== 'number'
+            || typeof num2 !== 'number'
+            || typeof num3 !== 'number'
         ) reject(new Error('Informe apenas números.'));
 
         const result = ((num1 + num2) * num3);
 
-        if (result < 50) reject(new Error('Valor muito baixo.'));
+        if (result < 5000) reject(new Error('Valor muito baixo.'));
 
         resolve(result);
     })
 
     return promise;
 }
+
+main(Math.floor(Math.random() * 100 + 1), Math.floor(Math.random() * 100 + 1), Math.floor(Math.random() * 100 + 1))
+    .then((data) => console.log(data))
+    .catch((err) => console.error(err.message));
