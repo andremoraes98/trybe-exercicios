ade01a8b1ca2ca6515189178c5163a4c6cfa7e2a
Andre Moraes <andre.moraes.98@gmail.com>
2022-06-07 19:09:34 -0300

    Implementação da parte 2 do exercício 4

--- 3 - Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolviment Web com Node.js/22.2 - Fluxo Assíncrono/exercicio/Parte_2/exercicio.js
+++ 3 - Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolviment Web com Node.js/22.2 - Fluxo Assíncrono/exercicio/Parte_2/exercicio.js
@@ -1,3 +1,25 @@
 const simpsonsJson = require('./simpsons.json');
+const readLine = require('readline-sync');
 
-simpsonsJson.map(person => console.log(`${person.id} - ${person.name}`));
+const personId = (id) => {
+    const promise = new Promise((resolve, reject) => {
+        if (!simpsonsJson.some(person => person.id == id)) reject(new Error('id não encontrado'));
+
+        resolve(simpsonsJson.filter(person => person.id == id));
+    })
+
+    return promise;
+}
+
+const id = readLine.questionInt('Qual o id do personagem que deseja retornar as informações? ')
+
+const main = async () => {
+    try {
+        const personInfo = await personId(id);
+        console.log(personInfo);
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+main();
