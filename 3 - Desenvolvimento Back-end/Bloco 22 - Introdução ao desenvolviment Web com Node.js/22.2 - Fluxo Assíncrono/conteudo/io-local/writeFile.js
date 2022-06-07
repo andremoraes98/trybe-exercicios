fe213d9d0058c141a043e311c57ea75965220379
Andre Moraes <andre.moraes.98@gmail.com>
2022-06-07 15:33:31 -0300

    Testando a flag wx quando escrever um arquivo

--- 3 - Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolviment Web com Node.js/22.2 - Fluxo Assíncrono/conteudo/io-local/writeFile.js
+++ 3 - Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolviment Web com Node.js/22.2 - Fluxo Assíncrono/conteudo/io-local/writeFile.js
@@ -1,9 +1,12 @@
 const fs = require('fs').promises;
 
-fs.writeFile('./meu-arquivo.txt', 'Meu textão')
+ // A flag ``´wx`` abre o arquivo para escrita **apenas** caso ele não exista. Caso o contrário, um erro será lançado:
+
+fs.writeFile('./meu-arquivo.txt', 'Eu estive aqui :eyes:', { flag: 'wx' })
   .then(() => {
-    console.log('Arquivo escrito com sucesso!');
+    console.log('Arquivo salvo');
   })
   .catch((err) => {
-    console.error(`Erro ao escrever o arquivo: ${err.message}`);
+    // Se o arquivo existir, um erro é retornado
+    console.error(err);
   });
