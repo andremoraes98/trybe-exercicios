d0df5ccd825249ecd5c4d213d1b1514db8f3c7c2
Andre Moraes <andre.moraes.98@gmail.com>
2022-06-10 19:04:23 -0300

    Adição da validação do campo info

--- 3 - Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolviment Web com Node.js/22.5 - Express: Middlewares/exercicio/index.js
+++ 3 - Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolviment Web com Node.js/22.5 - Express: Middlewares/exercicio/index.js
@@ -9,8 +9,10 @@ const products = [];
 const validateProductNameLength = (req, res, next) => {
   const { productName } = req.body;
 
-  if (productName.length < 4)
-    return res.status(400).json({ message: "O campo productName é muito curto!" });
+  if (productName.length < 4) {
+    res.status(400).json({ message: "O campo productName é muito curto!" });
+    return
+  }
 
   next();
 }
@@ -18,15 +20,41 @@ const validateProductNameLength = (req, res, next) => {
 const validateBlankProductName = (req, res, next) => {
   const { productName } = req.body;
 
-  if (!productName)
-    return res.status(400).json({ message: "O campo productName é obrigatório!" });
+  if (!productName) {
+    res.status(400).json({ message: "O campo productName é obrigatório!" });
+    return
+  }
 
   next();
 }
 
+const validateBlankinfo = (req, res, next) => {
+  const { infos } = req.body;
+
+  if (!infos) {
+    res.status(400).json({ message: "O campo infos é obrigatório!" });
+    return 
+  }
+
+  next();
+}
+
+const validateDate = (req, res, next) => {
+  const { infos : { saleDate } } = req.body;
+
+  if (!saleDate.match(/^(\d{1,2})\/(\d{1,2})\/(\d{4})$/)) {
+    res.status(400).json({ message: "Formato da saleDate é inválido!" });
+    return
+  }
+
+  next();
+} 
+
 app.post('/sales', [
   validateBlankProductName,
+  validateBlankinfo,
   validateProductNameLength,
+  validateDate,
   (req, res) => {
     const product = req.body;
   
