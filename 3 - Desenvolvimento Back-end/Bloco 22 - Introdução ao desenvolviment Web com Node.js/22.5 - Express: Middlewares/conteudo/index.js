9fba29eb27f61f41394d1e8f4fc39574128226ff
Andre Moraes <andre.moraes.98@gmail.com>
2022-06-10 14:58:45 -0300

    Aplicação de um middleware com o next

--- 3 - Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolviment Web com Node.js/22.5 - Express: Middlewares/conteudo/index.js
+++ 3 - Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolviment Web com Node.js/22.5 - Express: Middlewares/conteudo/index.js
@@ -10,6 +10,14 @@ const recipes = [
   { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
 ];
 
+const validatePrice = (req, res, next) => {
+  const { price } = req.body;
+
+  if (typeof price !== 'number' || Number(price) === 0) return res.status(400).json({ message: 'Invalid price!'});
+
+  next()
+};
+
 app.get('/recipes', function (req, res) {
   res.status(200).json(recipes);
 });
@@ -28,13 +36,13 @@ app.get('/recipes/:id', function (req, res) {
   res.status(200).json(recipe);
 });
 
-app.post('/recipes', function (req, res) {
+app.post('/recipes', validatePrice, function (req, res) {
   const { id, name, price, waitTime } = req.body;
   recipes.push({ id, name, price, waitTime});
   res.status(201).json({ message: 'Recipe created successfully!'});
 });
 
-app.put('/recipes/:id', function (req, res) {
+app.put('/recipes/:id', validatePrice, function (req, res) {
   const { id } = req.params;
   const { name, price, waitTime } = req.body;
   const recipeIndex = recipes.findIndex((r) => r.id === Number(id));
