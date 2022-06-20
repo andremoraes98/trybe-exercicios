bbb60b95178fd297698141a6cf116c9314e371ba
Andre Moraes <andre.moraes.98@gmail.com>
2022-06-20 14:49:43 -0300

    Conclusão do exercicio 2

--- 3 - Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolviment Web com Node.js/22.6 - Atividades + projeto/exercicio/index.js
+++ 3 - Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolviment Web com Node.js/22.6 - Atividades + projeto/exercicio/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const generateToken = require('./crypto');
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,6 +13,17 @@ app.post('/user/register', (req, res) => {
   }
 
   res.status(201).json({ "message": "user created" });
+});
+
+app.post('/user/login', (req, res) => {
+  const { email, password } = req.body;
+  const token = generateToken();
+  
+  if (password.length < 4 || password.length > 8 || typeof password !== 'number' || !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    return res.status(400).json({ "message": "email or password is incorrect" });
+  }
+
+  res.status(200).json({ token });
 });
 
 app.listen(3001, () => {
