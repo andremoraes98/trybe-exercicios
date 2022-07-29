f6de0d1f4f679fa32dafb6f9212ba439b14ac8a1
Andre Moraes <andre.moraes.98@gmail.com>
2022-07-29 10:00:14 -0300

    Implementação do getAll na camada model

---
+++ 3 - Desenvolvimento Back-end/Bloco 26 - TypeScript/26.3 - Express com TypeScript/models/users.model.ts
@@ -0,0 +1,18 @@
+import { Pool } from "mysql2/promise";
+import User from '../interfaces/users.interface'
+
+export default class BookModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async getAll():Promise<User[]> {
+    const result= await this.connection.query(`SELECT id, name, email, password FROM Users`);
+
+    const [rows] = result;
+
+    return rows as User[];
+  }
+}
