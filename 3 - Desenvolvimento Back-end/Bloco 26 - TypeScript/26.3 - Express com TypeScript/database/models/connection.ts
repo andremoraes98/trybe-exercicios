201d4777707ddb703dfafd97fbe83f4a7b024f62
Andre Moraes <andre.moraes.98@gmail.com>
2022-07-29 10:43:18 -0300

    Implementação do método GET /users

--- 3 - Desenvolvimento Back-end/Bloco 26 - TypeScript/26.3 - Express com TypeScript/database/models/connection.ts
+++ 3 - Desenvolvimento Back-end/Bloco 26 - TypeScript/26.3 - Express com TypeScript/database/models/connection.ts
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
-dotenv.config;
+dotenv.config();
 
 export default mysql.createPool({
   host: process.env.DB_HOST,
--- 3 - Desenvolvimento Back-end/Bloco 26 - TypeScript/26.3 - Express com TypeScript/index.ts
+++ 3 - Desenvolvimento Back-end/Bloco 26 - TypeScript/26.3 - Express com TypeScript/index.ts
@@ -1,15 +1,13 @@
 import express, { NextFunction, Request, Response} from 'express';
 import 'express-async-errors';
+import userRoutes from './routes/user.routes'
 
 const app = express();
 
 app.use(express.json());
+app.use('/users', userRoutes);
 
 const PORT = 8000;
-
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).send('Express + TypeScript')
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
