2d2d2a28c1ed0d908a9a0949c96dabb2ee88e4d6
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-02 19:18:26 -0300

    feat: implementação dos métodos sumNotes e averageNotes

--- 3 - Desenvolvimento Back-end/Bloco 27 - Programação Orientada a Objetos (POO) e SOLID/index.ts
+++ 3 - Desenvolvimento Back-end/Bloco 27 - Programação Orientada a Objetos (POO) e SOLID/index.ts
@@ -1,17 +1,40 @@
 class Student {
   public registration: string;
   public nome: string;
-  public math: number;
-  public port: number;
-  public geo: number;
-  public hist: number;
+  private math: number;
+  private port: number;
+  private geo: number;
+  private hist: number;
+  private trab1: number;
+  private trab2: number;
 
-  constructor(r: string, n: string, m: number, p: number, g: number, h: number) {
+  constructor(
+    r: string,
+    n: string,
+    m: number,
+    p: number,
+    g: number,
+    h: number,
+    t1: number,
+    t2: number,
+  ) {
     this.registration = r;
     this.nome = n;
     this.math = m;
     this.port = p;
     this.geo = g;
     this.hist = h;
+    this.trab1 = t1;
+    this.trab2 = t2;
+  }
+
+  public sumNotes = (): number => {
+    const sum = this.math + this.port + this.geo + this.hist + this.trab1 + this.trab2;
+    return sum;
+  }
+
+  public averageNotes = (): number => {
+    const average = (this.math + this.port + this.geo + this.hist + this.trab1 + this.trab2) / 6;
+    return average;
   }
 }
