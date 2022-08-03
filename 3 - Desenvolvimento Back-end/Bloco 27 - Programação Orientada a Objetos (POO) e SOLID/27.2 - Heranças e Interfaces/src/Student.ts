da0ca5dbafc40e661bb7291cbb710ce9b6d01e87
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-03 19:40:57 -0300

    feat: setup inicial

---
+++ 3 - Desenvolvimento Back-end/Bloco 27 - Programação Orientada a Objetos (POO) e SOLID/27.2 - Heranças e Interfaces/src/Student.ts
@@ -0,0 +1,40 @@
+export default class Student {
+  public registration: string;
+  public nome: string;
+  private _math: number;
+  private _port: number;
+  private _geo: number;
+  private _hist: number;
+  private _trab1: number;
+  private _trab2: number;
+
+  constructor(
+    registration: string,
+    nome: string,
+    math: number,
+    port: number,
+    geo: number,
+    hist: number,
+    work1: number,
+    work2: number,
+  ) {
+    this.registration = registration;
+    this.nome = nome;
+    this._math = math;
+    this._port = port;
+    this._geo = geo;
+    this._hist = hist;
+    this._trab1 = work1;
+    this._trab2 = work2;
+  }
+
+  public sumNotes = (): number => {
+    const sum = this._math + this._port + this._geo + this._hist + this._trab1 + this._trab2;
+    return sum;
+  }
+
+  public averageNotes = (): number => {
+    const average = (this._math + this._port + this._geo + this._hist + this._trab1 + this._trab2) / 6;
+    return average;
+  }
+}
