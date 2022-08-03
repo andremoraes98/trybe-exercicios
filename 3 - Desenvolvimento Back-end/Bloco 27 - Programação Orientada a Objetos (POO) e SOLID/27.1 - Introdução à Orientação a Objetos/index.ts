ce252c7f14e2732569388e0c1065a6d687ee5dab
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-03 09:04:25 -0300

    feat: Complementação da classe Order e adição do getter a classe ItemOrdered

--- 3 - Desenvolvimento Back-end/Bloco 27 - Programação Orientada a Objetos (POO) e SOLID/27.1 - Introdução à Orientação a Objetos/index.ts
+++ 3 - Desenvolvimento Back-end/Bloco 27 - Programação Orientada a Objetos (POO) e SOLID/27.1 - Introdução à Orientação a Objetos/index.ts
@@ -54,14 +54,39 @@ class ItemOrdered extends PersonLunch {
     super(name);
     this._price = price;
   }
+
+  get price() {
+    return this._price;
+  }
 }
 
 class Order {
   public person: PersonLunch;
   public items: ItemOrdered[];
+  public paymentType: string;
+  public percentDiscount: number;
 
-  constructor(person: PersonLunch, items: ItemOrdered[]) {
+  constructor(
+    person: PersonLunch,
+    items: ItemOrdered[],
+    paymentType: string,
+    percentDiscount: number,
+    ) {
     this.person = person;
     this.items = items;
+    this.paymentType = paymentType;
+    this.percentDiscount = percentDiscount;
+  }
+
+  public totalPayment(): number {
+    const total = this.items.reduce((acc, item) => acc += item.price, 0);
+
+    return total;
+  }
+
+  public totalPaymentWithDiscount(): number {
+    const total = this.totalPayment();
+    const percentTotalValue = 1 - (this.percentDiscount/100)
+    return total * percentTotalValue;
   }
 }
