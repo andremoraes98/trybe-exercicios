1d596524418055a0bbdb421d6e194daeafca0cc9
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-04 18:21:21 -0300

    feat: refatoração para a classe Person ser abstrata

--- 3 - Desenvolvimento Back-end/Bloco 27 - Programação Orientada a Objetos (POO) e SOLID/27.3 - Polimorfismo/src/index.ts
+++ 3 - Desenvolvimento Back-end/Bloco 27 - Programação Orientada a Objetos (POO) e SOLID/27.3 - Polimorfismo/src/index.ts
@@ -1,14 +1,5 @@
-import Teacher from './Teacher';
-import Subject from './Subject';
+import Person from "./Person";
 
-const math = new Subject('Matemática');
-const hist = new Subject('História');
-const phi = new Subject('Filosofia');
+const andre = new Person('André', '12/06/1998');
 
-const julia = new Teacher('Júlia', '04/10/2000', math, 10000, '04/10/2000');
-const andre = new Teacher('André', '12/06/1998', hist, 10000, '12/06/1998');
-const luciene = new Teacher('Luciene', '15/11/1963', phi, 10000, '15/11/1963');
-
-console.log(julia);
 console.log(andre);
-console.log(luciene);
