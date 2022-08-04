5302b860e081943cb0e51635522c4e8f79a8c41d
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-04 19:13:42 -0300

    refactor: transformação de interface em classe

--- 3 - Desenvolvimento Back-end/Bloco 27 - Programação Orientada a Objetos (POO) e SOLID/27.3 - Polimorfismo/src/Employee.ts
+++ 3 - Desenvolvimento Back-end/Bloco 27 - Programação Orientada a Objetos (POO) e SOLID/27.3 - Polimorfismo/src/Employee.ts
@@ -1,6 +1,48 @@
-export default interface Employee {
-  registration: number;
-  salary: number;
-  admissionDate: Date;
-  generateRegistration(): number; 
+import Enrollable from "./Enrollable";
+import Person from "./Person";
+
+export default abstract class Employee extends Person implements Enrollable {
+  private salary: number;
+  private admissionDate: Date;
+
+  constructor(nome: string, birthDate: string, salary: number, admissionDate: string) {
+    super(nome, birthDate);
+    this.enrollment = this.generateEnrollment();
+    
+    this.isSalaryValid(salary)
+      ? this.salary = salary
+      : this.salary = 0;
+    this.isAdmissionDateValid(admissionDate)
+      ? this.admissionDate = new Date(admissionDate)
+      : this.admissionDate = new Date('01/01/2000');
+  }
+
+  get enrollment(): string {
+    return this.enrollment
+  }
+
+  set enrollment(newValue: string) {
+    newValue.length >= 16 
+      ? this.enrollment = newValue
+      : null
+  }
+
+  public generateEnrollment(): string {
+    const actualYear = new Date().getFullYear();
+    const randomNumber = (Math.random() * 1000000000000).toFixed(0);
+    const stringRegistration = `${actualYear}${randomNumber}`
+
+    return stringRegistration;
+  }
+
+  private isAdmissionDateValid(date: string): boolean {
+    const today = new Date().getTime();
+    const newDate = new Date(date).getTime();
+
+    return newDate < today;
+  }
+
+  private isSalaryValid(salary: number): boolean {
+    return salary > 0;
+  }
 };
